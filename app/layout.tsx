1358c6a83982ac006f5abf9ab2257bbed1e2ebe4
aldojeda92-spec <aldojeda92@gmail.com>
2026-08-17 15:19:02 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-52KCG6F3');`
+          }}
+        />
+      </head>
       <body className={`${montserrat.variable} ${inter.variable} font-inter bg-white text-dataCharcoal antialiased selection:bg-digitalCyan selection:text-authorityBlue`}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-52KCG6F3"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         {/* El Provider envuelve toda la app, inyectando el Modal una sola vez en el DOM */}
         <ToastProvider>
           <LeadProvider>
